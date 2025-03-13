714e5e072f1b5e45b75d00ecc8464540ba409e11
ROGGIE018\sofia <223A1020@live.uem.es>
2025-03-12 23:54:15 -0500

    sprites all

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,20 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public float jumpForce;
     public float groundDist;
 
     public LayerMask groundMask;
     public Rigidbody rb;
     public SpriteRenderer SpriteRenderer;
+    public SpriteRenderer sideSpite;
+    public SpriteRenderer frontSprite;
 
-
+    private bool isGrounded;
 
     void Start()
     {
@@ -21,7 +25,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, groundDist, groundMask);
        
 
         float x = Input.GetAxis("Horizontal");
@@ -39,6 +43,10 @@ public class PlayerController : MonoBehaviour
         else if (x != 0 && x > 0)
         {
             SpriteRenderer.flipX = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
         }
     }
 }
