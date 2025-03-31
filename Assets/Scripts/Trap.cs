eef51ed3b61ac044e9d498e6c2bae719696954a8
ROGGIE018\sofia <223A1020@live.uem.es>
2025-03-31 10:06:01 -0500

    ccodigo enemigo

--- Assets/Scripts/Trap.cs
+++ Assets/Scripts/Trap.cs
@@ -3,15 +3,25 @@ using UnityEngine;
 public class Trap : MonoBehaviour
 {
     public int damage;
+    float pushForce = 5f;
 
     private void OnTriggerEnter(Collider other)
     {
 
         HealthControl healthControl = other.GetComponent<HealthControl>();
+        Rigidbody rb = other.GetComponent<Rigidbody>();
         if (healthControl != null)
         {
             healthControl.TakeDamage(damage);
             Debug.Log("vida del jugador:" + healthControl.health);
+
+            if (rb != null)
+            {
+                Vector3 pushDirection = (other.transform.position - transform.position).normalized;
+                pushDirection.y = 0;
+                rb.AddForce(pushDirection* pushForce, ForceMode.Impulse);
+            }
+
         }
 
     }
